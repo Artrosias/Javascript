610f50ed0099582277b0687a0dba08d7b08a23e4
Alessio Guidotti <48104320+Artrosias@users.noreply.github.com>
2021-01-25 17:19:27 +0100

    Add files via upload

---
+++ esercizio.js
@@ -0,0 +1,10 @@
+var punteggioGiocatoreA = (10 * 3) + 3;
+console .log(punteggioGiocatoreA);
+var punteggioGiocatoreB = (7 * 3) + 8;
+console .log(punteggioGiocatoreB);
+
+
+var punteggioMiglioreA = punteggioGiocatoreA > punteggioGiocatoreB;
+console .log(punteggioMiglioreA);
+
+console .log("Giocatore A ha ottenuto piu punti di giocatore B?"  +  punteggioMiglioreA );
