29f4961378aac11fc9dc1207470cf8ef5a4462f0
Alessio Guidotti <48104320+Artrosias@users.noreply.github.com>
2021-01-27 10:46:56 +0100

    Add files via upload

---
+++ variabili.js
@@ -0,0 +1,24 @@
+// Variabili 
+
+// var, let, conts
+
+// var eta = 21;
+
+// Sintassi variabili
+
+// lettere, numeri, _, $
+
+// var nome =  21;
+// console .log("eta");
+
+
+// Variabili pt2
+
+// prima 2015 var, 
+// dopo 2015 let, const
+
+// const nome = "alessio"; // COSTANTE
+// console .log(nome);
+
+// nome = "billy";  // SULLA CONSOLE DA ERRORE PERCHE IL VALORE DEVE ESSERE COSTANTE 
+// console .log(nome);
